bd9f329a8682231cd641e0d4c0d6114eeee803b4
Alexey Plotnikov <mr.plotnikov@gmail.com>
2025-03-24 14:40:47 +0700

    add playwright support

--- runner/home/ubuntu/gradle-template/build.gradle.kts
+++ runner/home/ubuntu/gradle-template/build.gradle.kts
@@ -1,3 +1,17 @@
 plugins {
     application
 }
+
+repositories {
+    mavenCentral()
+}
+
+// Usage: ./gradlew playwright --args="help"
+tasks.register<JavaExec>("playwright") {
+    classpath(sourceSets["test"].runtimeClasspath)
+    mainClass.set("com.microsoft.playwright.CLI")
+}
+
+dependencies {
+    implementation("com.microsoft.playwright:playwright:1.51.0")
+}
